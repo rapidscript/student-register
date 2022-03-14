a7ea44a15b657a43cfb1780f3354db1d8aaac2e7
rapidscript <rapidscript@users.noreply.github.com>
2022-03-14 05:43:50 +0300

    changed port to https

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const port = 80;
+const port = 443;
 const jsonParser = express.json();
 const fileName = 'students.json';
 
